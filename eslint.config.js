98022afe163ef87f9f9900a5d772543b78b3f8ce
SVS Enterprises <svs.enterprises.india@gmail.com>
2025-04-09 17:29:38 +0530

    Update: Upgrade dependencies and fix deprecated packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,53 @@
-import js from '@eslint/js';
 import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import * as parser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
-  { ignores: ['dist'] },
-  js.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist/**']
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      parser: parser,
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 'latest',
         sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
       },
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node
+      }
     },
     plugins: {
       '@typescript-eslint': tsPlugin,
+      'react': reactPlugin,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
     },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
   }
 ];
